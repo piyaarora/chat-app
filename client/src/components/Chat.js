cb50bbc4161d52540ceb3cdb5eb2da3cca27c5bc
piyaarora <arorapiya141@gmail.com>
2020-06-27 18:23:13 +0530

    endpoint changed

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -15,7 +15,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
-    const ENDPOINT = 'localhost:5000'
+    const ENDPOINT = 'https://chat-discussion-room.herokuapp.com/'
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
