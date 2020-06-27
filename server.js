fbbecb58e4624d0cb4c82161ce61968e2f99b401
piyaarora <arorapiya141@gmail.com>
2020-06-27 16:28:05 +0530

    serverjs updated

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ io.on('connect', (socket) => {
     })
 });
 
-if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-}
+// set static folder
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
